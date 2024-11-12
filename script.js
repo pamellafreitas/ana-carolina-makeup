5b0fef8acaca0ac78aaa56b57a9d452bdacb5500
Pamella Freitas <pamellafreitas.dev@gmail.com>
2024-11-12 11:20:09 -0300

    css modificado

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 function toggleImages(sectionId, button, event) {
-    event.preventDefault(); // Previne o comportamento padrão do link
+    event.preventDefault(); 
   
     const section = document.getElementById(sectionId);
     const images = section.querySelectorAll('.additional-image');
   
-    // Alterna entre mostrar e esconder as imagens adicionais
     images.forEach(image => image.classList.toggle('d-none'));
   
-    // Muda o texto do botão com base na visibilidade das imagens
+  
     if (button.textContent === 'VER MAIS') {
       button.textContent = 'VER MENOS';
     } else {
