937a063c11b35e4ec922d2c4f79e9553e5aea23d
Pamella Freitas <pamellafreitas.dev@gmail.com>
2024-11-13 10:31:08 -0300

    Correção

--- script.js
+++ script.js
@@ -13,7 +13,35 @@ function toggleImages(sectionId, button, event) {
       button.textContent = 'VER MAIS';
     }
   }
+
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const navLinks = document.querySelectorAll(".navbar-nav .nav-link");
+    const navItems = document.querySelectorAll('.nav-item');
+    const navbarCollapse = document.getElementById("navbarNav");
   
+    // Adiciona o evento de clique a cada item de navegação
+    navItems.forEach(function(item) {
+      item.addEventListener('click', function () {
+        // Remove a classe 'active' de todos os itens do menu
+        navItems.forEach(function(i) {
+          i.classList.remove('active');
+        });
+        // Adiciona a classe 'active' ao item clicado
+        item.classList.add('active');
+  
+        // Verifica se o menu está aberto e fecha se necessário
+        if (navbarCollapse.classList.contains("show")) {
+          $('.navbar-collapse').collapse('hide'); // Fecha o menu em dispositivos móveis
+        }
+      });
+    });
+  });
+  
+  
+  
+
+
   
   
 
